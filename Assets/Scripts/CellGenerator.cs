9e6948314996b5321980605bd55d0fc28d5b9975
SrMeuNome <vinicius10001999@gmail.com>
2021-06-07 01:14:00 -0300

    Fix bug rule top cell

--- Assets/Scripts/CellGenerator.cs
+++ Assets/Scripts/CellGenerator.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class GeradorCelular : MonoBehaviour
-{
-    //Se a celula for preenchida e não tiver nenhum vizinho preenchido a mesma passará a não ser preenchida
-    //Se a celula for preenchida com um determinado tipo X e a maior parte dos vizinhos for preenchidos com o tipo Y a celula se torna do tipo Y
-    //Se a celula for preenchida com um tipo X e tiver dois vizinhos ou menos com o tipo Y a celula se manterá com o tipo X
-    //Se a celula for preenchida e não tiver nada em cima ela será uma celula de topo do tipo correspondente
-    //Se a celula for vazia e tiver dois ou mais vizinhos cada um de um tipo ela se tornara um dos tipos com a probabilidade 1/n
-    //Se a celula for vazia e tiver dois ou mais vizinhos ela terá o mesmo estado do vizinho que mais repete
+//Se a celula for preenchida e não tiver nenhum vizinho preenchido a mesma passará a não ser preenchida
+//Se a celula for preenchida com um determinado tipo X e a maior parte dos vizinhos for preenchidos com o tipo Y a celula se torna do tipo Y
+//Se a celula for preenchida com um tipo X e tiver dois vizinhos ou menos com o tipo Y a celula se manterá com o tipo X
+//Se a celula for preenchida e não tiver nada em cima ela será uma celula de topo do tipo correspondente
+//Se a celula for vazia e tiver dois ou mais vizinhos cada um de um tipo ela se tornara um dos tipos com a probabilidade 1/n
+//Se a celula for vazia e tiver dois ou mais vizinhos ela terá o mesmo estado do vizinho que mais repete
 
+public class CellGenerator : MonoBehaviour
+{
 
     //Tamanho do mapa
     public int width;
@@ -23,23 +23,15 @@ public class GeradorCelular : MonoBehaviour
     public Tilemap tilemapBase;
     public Tilemap tilemapAux;
 
-    //TileBase é o tile que queremos utilizar
-    [System.Serializable]
-    public class TileBaseGroup
-    {
-        [SerializeField]
-        public TileBase tileTop;
-
-        [SerializeField]
-        public TileBase tileDefault;
-    }
-
     public List<TileBaseGroup> listTileBase = new List<TileBaseGroup>();
+
+    private bool run = false;
 
     // Start is called before the first frame update
     void Start()
     {
         TileBaseSort();
+        //Smooth();
         for (int i = 0; i < 30; i++)
         {
             Smooth();
@@ -49,8 +41,11 @@ public class GeradorCelular : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Smooth();
-        //tilemapBase.RefreshAllTiles();
+        /*if (!run)
+        {
+            print("Rodando");
+            Smooth();
+        }*/
     }
 
     public void TileBaseSort()
@@ -59,9 +54,9 @@ public class GeradorCelular : MonoBehaviour
         {
             for (int j = startPosition; j < width; j++)
             {
-                int sortTilePosition = Random.Range(-1, listTileBase.Count);
+                int sortTilePosition = Random.Range(-listTileBase.Count, listTileBase.Count);
 
-                //Atribuindo tilebase sotiada
+                //Atribuindo tilebase sortida
                 if (sortTilePosition >= 0)
                 {
                     TileBase tileBaseSelected = listTileBase[sortTilePosition].tileDefault;
@@ -73,6 +68,7 @@ public class GeradorCelular : MonoBehaviour
 
     public void Smooth()
     {
+        run = true;
         for (int i = startPosition; i < height; i++)
         {
             for (int j = startPosition; j < width; j++)
@@ -80,38 +76,39 @@ public class GeradorCelular : MonoBehaviour
                 if(IsAroundVoid(tilemapBase, j, i))
                 {
                     tilemapAux.SetTile(new Vector3Int(j, i, 0), null);
-                }
-
-                if(HasThreeOrMore(tilemapBase, j, i) != null)
+                } else if(HasThreeOrMore(tilemapBase, j, i) != null)
                 {
                     tilemapAux.SetTile(new Vector3Int(j, i, 0), HasThreeOrMore(tilemapBase, j, i));
-                }
-                else
+                } else
                 {
                     tilemapAux.SetTile(new Vector3Int(j, i, 0), tilemapBase.GetTile(new Vector3Int(j, i, 0)));
                 }
 
-                if(IsTop(tilemapBase, j, i))
+                if (tilemapAux.HasTile(new Vector3Int(j, i, 0)))
                 {
-                    TileBaseGroup auxTileSetTopGroup = listTileBase.Find((tile) =>
-                    tile.tileDefault.Equals(tilemapBase.GetTile(new Vector3Int(j, i, 0)))
-                    || tile.tileTop.Equals(tilemapBase.GetTile(new Vector3Int(j, i, 0))));
-
-                    if(auxTileSetTopGroup != null)
+                    if (IsTop(tilemapBase, j, i))
                     {
-                        TileBase auxTileSetTop = auxTileSetTopGroup.tileTop;
-                        tilemapAux.SetTile(new Vector3Int(j, i, 0), auxTileSetTop);
+                        TileBaseGroup auxTileSetTopGroup = listTileBase.Find((tile) =>
+                        tile.tileDefault.Equals(tilemapBase.GetTile(new Vector3Int(j, i, 0)))
+                        || tile.tileTop.Equals(tilemapBase.GetTile(new Vector3Int(j, i, 0))));
+
+                        if (auxTileSetTopGroup != null)
+                        {
+                            TileBase auxTileSetTop = auxTileSetTopGroup.tileTop;
+                            tilemapAux.SetTile(new Vector3Int(j, i, 0), auxTileSetTop);
+                        }
                     }
-                } else
-                {
-                    TileBaseGroup auxTileSetDefaultGroup = listTileBase.Find((tile) =>
-                    tile.tileDefault.Equals(tilemapBase.GetTile(new Vector3Int(j, i, 0)))
-                    || tile.tileTop.Equals(tilemapBase.GetTile(new Vector3Int(j, i, 0))));
-
-                    if (auxTileSetDefaultGroup != null)
+                    else
                     {
-                        TileBase auxTileSetDefault = auxTileSetDefaultGroup.tileDefault;
-                        tilemapAux.SetTile(new Vector3Int(j, i, 0), auxTileSetDefault);
+                        TileBaseGroup auxTileSetTopGroup = listTileBase.Find((tile) =>
+                        tile.tileDefault.Equals(tilemapBase.GetTile(new Vector3Int(j, i, 0)))
+                        || tile.tileTop.Equals(tilemapBase.GetTile(new Vector3Int(j, i, 0))));
+
+                        if (auxTileSetTopGroup != null)
+                        {
+                            TileBase auxTileSetDefault = auxTileSetTopGroup.tileDefault;
+                            tilemapAux.SetTile(new Vector3Int(j, i, 0), auxTileSetDefault);
+                        }
                     }
                 }
             }
@@ -124,6 +121,7 @@ public class GeradorCelular : MonoBehaviour
                 tilemapAux.SetTile(new Vector3Int(j, i, 0), null);
             }
         }
+        run = false;
     }
 
     private bool IsAroundVoid(Tilemap tilemap, int x, int y)
@@ -246,4 +244,15 @@ public class GeradorCelular : MonoBehaviour
             return false;
         }
     }
+}
+
+//TileBase é o tile que queremos utilizar
+[System.Serializable]
+public class TileBaseGroup
+{
+    [SerializeField]
+    public TileBase tileTop;
+
+    [SerializeField]
+    public TileBase tileDefault;
 }
